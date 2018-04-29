0c7542741b933adb3c4e5194c817c5c4cbd5506b
Diego Rueda <diego.rueda2@gmail.com>
2018-04-29 11:36:56 -0400

    add session and init lavel collections

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,6 +11,12 @@ class HomeController extends Controller
 	 * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
 	 */
 	public function index () {
+	    if(auth()->check()){
+	        if(!session()->has('cart')){
+	            session()->put('cart', new Collection());
+	            session()->save();
+            }
+        }
         return view('home');
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,12 @@
 @extends('layouts.admin')
 
 @section('content')
+
+    @auth
+        <cart :user_id="{{ json_encode(auth()->id()) }}"></cart>
+    @endauth
+
     <promotions :logged="{{ json_encode(auth()->check()) }}"></promotions>
+    <products :logged="{{ json_encode(auth()->check()) }}"></products>
+
 @endsection
