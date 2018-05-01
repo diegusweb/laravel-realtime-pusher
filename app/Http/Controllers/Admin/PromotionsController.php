b6388445aedcb43d0e1450753e2b179e9bd08255
Diego Rueda <diego.rueda2@gmail.com>
2018-04-30 20:04:37 -0400

    Permtir al admin agragar nuevas promocones

--- app/Http/Controllers/Admin/PromotionsController.php
+++ app/Http/Controllers/Admin/PromotionsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Http\Requests\PromotionRequest;
 use App\Promotion;
 use App\VueTables\EloquentVueTables;
 use Illuminate\Http\Request;
@@ -10,7 +11,7 @@ use App\Http\Controllers\Controller;
 class PromotionsController extends Controller
 {
     public function index(){
-        return view('admin.promotions');
+        return view('admin.promotions.index');
     }
 
     public function json(){
@@ -21,5 +22,16 @@ class PromotionsController extends Controller
         }
 
         abort(401);
+    }
+
+    public function create(){
+        $btnText = 'Crear promocion';
+        $route = route('admin.promotions_store');
+        return view('admin.promotions.create', compact('btnText', 'route'));
+    }
+
+    public function store(PromotionRequest $promotionRequest){
+        $promotion = Promotion::create($promotionRequest->input());
+        return back()->with('message', ['success', __('Promocion dada de alta correctamnee')]);
     }
 }
