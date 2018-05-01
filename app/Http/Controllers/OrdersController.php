e3344ef5ad4c265241b4266ca62127f86e9db909
Diego Rueda <diego.rueda2@gmail.com>
2018-05-01 14:25:32 -0400

    componente para mostrar los pedids al cliente

---
+++ app/Http/Controllers/OrdersController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Order;
+use App\VueTables\EloquentVueTables;
+use Illuminate\Http\Request;
+
+class OrdersController extends Controller
+{
+    public function index () {
+        return view('orders.index');
+    }
+
+    public function json () {
+        if(request()->ajax()) {
+            $vueTables = new EloquentVueTables;
+            $data = $vueTables->get(new Order, ['id', 'user_id', 'total', 'status'], ['orderLines'], true);
+            return response()->json($data);
+        }
+        return abort(401);
+    }
+
+    public function jsonDetail ($id) {
+        $order = Order::with('orderLines.product')->findOrFail($id);
+        return response()->json($order);
+    }
+
+    public function show ($orderId) {
+        return view('orders.detail', compact( 'orderId'));
+    }
+}
